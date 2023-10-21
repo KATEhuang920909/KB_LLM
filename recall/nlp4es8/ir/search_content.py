a041ab65a5bf397fcdfc33e4d67e33bfbee8c244
KATEhuang920909 <18707125049@163.com>
2023-10-21 18:00:04 +0800

    update

--- recall/nlp4es8/ir/search_content.py
+++ recall/nlp4es8/ir/search_content.py
@@ -59,27 +59,28 @@ class Search(object):
 if __name__ == '__main__':
     import pandas as pd
     import json
+    from tqdm import tqdm
 
     index_file_content_config = Config()
     index_file_content_config.index_name = "kbqa"
     index_file_content_config.top_n = 30
     search_name = Search(index_file_content_config, "kbqa")
     #
-    # predict_data = open("../../../result/test_tuple_extract_v2.json", encoding="utf8").readlines()
-    # submission = open("../../../result/es_llm_top20_test_quchong_v2.json", "w", encoding="utf8")
-    # predict_data = eval("".join([k.strip() for k in predict_data]))
-    # print(predict_data[0])
-    # final_result = []
-    from tqdm import tqdm
+    predict_data = open(r"D:\compet\基于通用大模型的知识库问答\KB_LLM\result\qa/test_tuple_extract_qa.json",
+                        encoding="utf8").readlines()
+    submission = open(r"D:\compet\基于通用大模型的知识库问答\KB_LLM\result\qaes_llm_top20_test_quchong_qa.json", "w",
+                      encoding="utf8")
+    predict_data = eval("".join([k.strip() for k in predict_data]))
+    print(predict_data[0])
+    final_result = []
 
-    #
-    # for unit in tqdm(predict_data):
-    #     query = "".join(unit["attribute"])
-    #     result = search_name.searchAnswer(query)
-    #     temp_final_result = [k[2] for k in result]
-    #     unit["top20"] = temp_final_result
-    #     final_result.append(unit)
-    # submission.write(json.dumps(final_result, ensure_ascii=False))
+    for unit in tqdm(predict_data):
+        query = "".join(unit["attribute"])
+        result = search_name.searchAnswer(query)
+        temp_final_result = [k[2] for k in result]
+        unit["top30"] = temp_final_result
+        final_result.append(unit)
+    submission.write(json.dumps(final_result, ensure_ascii=False))
 
     # while True:
     #     query = input("please input")
@@ -87,7 +88,6 @@ if __name__ == '__main__':
     #     # print(result[0])
     #     for data in result:
     #         print("question:%s  idx_document:%s ori_document:%s " % (data[0], data[1], data[2]))
-
 
     # train_data = [json.loads(k) for k in open("../../../data/train_sft.json", encoding="utf8")]
     # dev_data = [json.loads(k) for k in open("../../../data/dev_sft.json", encoding="utf8")]
@@ -113,13 +113,13 @@ if __name__ == '__main__':
     #     dev_final_result.append(unit)
     # dev_submission.write(json.dumps(dev_final_result, ensure_ascii=False))
 
-    data = train_data=json.loads(open("../../../data/data_sft_top20.json",encoding="utf8").read())
-    data_submission = open("../../../data/data_sft_top20_v3.json", "w", encoding="utf8")
-    data_final_result = []
-    for unit in tqdm(data):
-        query = ",".join(["".join([k.strip() for k in u.split("|||")[:2]]) for u in unit["attribute"]])
-        result = search_name.searchAnswer(query)
-        temp_final_result = [k[2] for k in result]
-        unit["top20"] = temp_final_result
-        data_final_result.append(unit)
-    data_submission.write(json.dumps(data_final_result, ensure_ascii=False))
+    # data = train_data=json.loads(open("../../../data/data_sft_top20.json",encoding="utf8").read())
+    # data_submission = open("../../../data/data_sft_top20_v3.json", "w", encoding="utf8")
+    # data_final_result = []
+    # for unit in tqdm(data):
+    #     query = ",".join(["".join([k.strip() for k in u.split("|||")[:2]]) for u in unit["attribute"]])
+    #     result = search_name.searchAnswer(query)
+    #     temp_final_result = [k[2] for k in result]
+    #     unit["top20"] = temp_final_result
+    #     data_final_result.append(unit)
+    # data_submission.write(json.dumps(data_final_result, ensure_ascii=False))
